32f4d4a96f165c774d0863eb99908b4ea535fe75
Raphael 'mhmnn' Sahann <raphael@sahann.at>
2015-08-29 18:06:12 +0200

    worker now produces nodes

--- orgviz-js/lib/worker.js
+++ orgviz-js/lib/worker.js
@@ -1,47 +1,46 @@
-var httpRequest; //{{{
-function makeRequest(url, readystatechange) {
-  if (window.XMLHttpRequest) { // Mozilla, Safari, ...
-    httpRequest = new XMLHttpRequest();
-  } else if (window.ActiveXObject) { // IE
-    try {
-      httpRequest = new ActiveXObject("Msxml2.XMLHTTP");
-    } 
-    catch (e) {
-      try {
-        httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
-      } 
-      catch (e) {}
+var orgfile = "http://cpee.org/~demo/orgviz/organisation_informatik.xml";
+
+var GraphWorker = function(file,xpath,subjects,nopts){
+  this.nodes = [];
+  
+  function handler(response) {
+    if(this.status == 200 && this.responseXML != null )
+    {
+      console.log(this.responseXML);
+      processData(this.responseXML);
+    } else {
+      console.log("schas")
     }
   }
 
-  if (!httpRequest) {
-    //alert('Giving up :( Cannot create an XMLHTTP instance');
-    return false;
+  var client = new XMLHttpRequest();
+  var response;
+  client.onload = handler(response);
+  client.open("GET", orgfile, true);
+  client.send(null);
+
+  function nsResolver(prefix) {
+    return prefix == 'o' ? "http://cpee.org/ns/organisation/1.0" : null ;
   }
-  httpRequest.onreadystatechange = readystatechange;
-  httpRequest.open('GET', url);
-  httpRequest.send();
-} //}}}
 
-var orgfile = "https://raph.cs.univie.ac.at/organisation.xml";
+  function processData(data) {
+    var evalue = data.evaluate('/o:organisation/o:units/o:unit|/o:organisation/o:roles/o:role',
+                               data,
+                               nsResolver,
+                               XPathResult.ANY_TYPE,
+                               null);
+    console.log("blubb")
+    var node = evalue.iterateNext();
+    for(; node && !node.invalidIteratorState; ) {
+        var curr = Node(node.id, node.prefix ? node.prefix + ":" : "" + node.localName, nopts);
+        nodes.push(curr);
+        node = evalue.iterateNext()
+    }
+    console.log(nodes);
+    console.log(ids);
+    console.log(types);
+  }
 
-var GraphWorker = function(file,xpath,subjects,nopts){
-    this.nodes = [];
-
-    var schema;
-    function setSchema() {
-      if (httpRequest.readyState === 4) {
-        if (httpRequest.status === 200) {
-          schema = responseText;
-        } else {
-          schema = null;
-        }
-      }
-    } 
-    
-    makeRequest(orgfile, setSchema);
-    if(!schema) return false;
-    // validate Schema?
 
 }
 
