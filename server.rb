7ae585f2dcca305c628c68721e6a577852064d24
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2016-09-21 14:10:05 +0200

    better. everything.

--- server.rb
+++ server.rb
@@ -7,8 +7,8 @@ require 'riddl/client'
 require 'riddl/utils/notifications_producer'
 require 'riddl/utils/fileserve'
 
-port = File.read(File.dirname(__FILE__)+'/port')
-lh =   File.read(File.dirname(__FILE__)+'/localhost')
+port = File.read(File.dirname(__FILE__)+'/port').strip
+lh =   File.read(File.dirname(__FILE__)+'/localhost').strip
 
 class Continue #{{{
   def initialize
@@ -137,6 +137,10 @@ class ActivityHappens < Riddl::Implementation #{{{
     activity['url'] = @h['CPEE_CALLBACK']
     activity['id']  = @h['CPEE_CALLBACK'].split('/').last
 
+    activity['cpee_activity_id'] = @h['CPEE_ACTIVITY']
+    activity['cpee_base'] = @h['CPEE_BASE']
+    activity['cpee_instance'] = @h['CPEE_INSTANCE']
+
     omo = @p.shift.value
     activity['orgmodel'] = @h[ 'CPEE_ATTR_' + omo.upcase] || omo
 
@@ -177,13 +181,13 @@ class ActivityHappens < Riddl::Implementation #{{{
       @a[0].add_activity domain, activity
       @a[0][domain].add_orgmodel Riddl::Protocols::Utils::escape(activity['orgmodel']), xml
       Thread.new do
-        results = @a[0][domain].vote('task/add', :user => user , :cpee_callback => @h['CPEE_CALLBACK'], :cpee_instance => @h['CPEE_INSTANCE'], :cpee_base => @h['CPEE_BASE'], :cpee_label => @h['CPEE_LABEL'], :cpee_activity => @h['CPEE_ACTIVITY'])
+        results = @a[0][domain].vote('task/add', :user => user ,                                :cpee_callback => activity['url'], :cpee_instance => activity['cpee_instance'], :cpee_base => activity['cpee_base'], :cpee_label => activity['label'], :cpee_activity => activity['cpee_activity_id'])
         if (results.length == 1) && (user.include? results[0])
           activity["user"] = results[0]
-          @a[0][domain].notify('task/add', :user => user , :cpee_callback => @h['CPEE_CALLBACK'], :cpee_instance => @h['CPEE_INSTANCE'], :cpee_base => @h['CPEE_BASE'], :cpee_label => @h['CPEE_LABEL'], :cpee_activity => @h['CPEE_ACTIVITY'])
-          @a[0][domain].notify('user/take', :index => activity['id'], :user => results[0])
+          @a[0][domain].notify('task/add',       :user => user,                                 :cpee_callback => activity['url'], :cpee_instance => activity['cpee_instance'], :cpee_base => activity['cpee_base'], :cpee_label => activity['label'], :cpee_activity => activity['cpee_activity_id'])
+          @a[0][domain].notify('user/take',      :user => results[0], :index => activity['id'], :cpee_callback => activity['url'], :cpee_instance => activity['cpee_instance'], :cpee_base => activity['cpee_base'], :cpee_label => activity['label'], :cpee_activity => activity['cpee_activity_id'])
         else
-          @a[0][domain].notify('task/add', :user => user , :cpee_callback => @h['CPEE_CALLBACK'], :cpee_instance => @h['CPEE_INSTANCE'], :cpee_base => @h['CPEE_BASE'], :cpee_label => @h['CPEE_LABEL'], :cpee_activity => @h['CPEE_ACTIVITY']) if @a[0].keys.include? domain
+          @a[0][domain].notify('task/add',       :user => user,                                 :cpee_callback => activity['url'], :cpee_instance => activity['cpee_instance'], :cpee_base => activity['cpee_base'], :cpee_label => activity['label'], :cpee_activity => activity['cpee_activity_id']) if @a[0].keys.include? domain
         end
       end
       @headers << Riddl::Header.new('CPEE_CALLBACK','true')
@@ -282,7 +286,7 @@ class TaskTake < Riddl::Implementation #{{{
       @a[0].activities[index]["user"] = @r[-3]
       callback_id = @a[0].activities[index]['id']
       @a[0].activities.serialize
-      @a[0].notify('user/take', :index => callback_id, :user => @r[-3])
+      @a[0].notify('user/take', :user => @r[-3], :index => callback_id, :cpee_callback => activity['url'], :cpee_instance => activity['cpee_instance'], :cpee_base => activity['cpee_base'], :cpee_label => activity['label'], :cpee_activity => activity['cpee_activity_id'])
       Riddl::Client.new(@a[0].activities[index]['url']).put [
         Riddl::Header.new('CPEE_UPDATE','true'),
         Riddl::Header.new('CPEE_UPDATE_STATUS','take')
