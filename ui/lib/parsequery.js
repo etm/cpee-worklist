f247fac174e1b21e2317645e302b80e383d59892
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2024-02-16 09:26:01 +0100

    cleanup old libs

--- ui/lib/parsequery.js
+++
@@ -1,8 +0,0 @@
-$.parseQuery = function(querystring) {
-  var ret = [];
-  querystring.replace(/#.*$/,'');
-  querystring.replace(/([^&=]+)=?([^&]*)(?:&+|$)/g, function(match, key, value) {
-    ret.push( { 'key': key, 'value': value  });
-  });
-  return ret;
-}
